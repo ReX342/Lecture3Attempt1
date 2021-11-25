eefa76e15ef796afb171054cda9b74a838be9d57
ReX342 <occ.rex@gmail.com>
2021-11-25 23:17:46 +0100

    urlpatterns.oneword.donttypebyheart

--- assignment/SolutionSet/LectureDjango/Lecture3/newyear/views.py
+++ assignment/SolutionSet/LectureDjango/Lecture3/newyear/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    now = datetime.now()
+    context = {
+        "newyear": now.month == 1 and now.day == 1
+    }
+    return render(request, "newyear/index.html", context)
