3357774335d4034f11aa44a1e0c5359efb6109c0
ReX342 <occ.rex@gmail.com>
2021-11-26 00:07:58 +0100

    namespace order path confusion resolved with app

--- assignment/wiki/encyclopedia/urls.py
+++ assignment/wiki/encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.watch, name="watch"),
     path("create", views.create_entry, name="create"),
-    path("random", views.random, name="random")
+    path("random", views.random, name="random"),
+    path("add", views.add, name="add"),
 ]
