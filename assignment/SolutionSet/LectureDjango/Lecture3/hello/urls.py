fede562428703cb471c74c641ddfa683eab5b641
ReX342 <occ.rex@gmail.com>
2021-11-25 23:04:06 +0100

    greet works with context

--- assignment/SolutionSet/LectureDjango/Lecture3/hello/urls.py
+++ assignment/SolutionSet/LectureDjango/Lecture3/hello/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
-    path("brian", views.brian)
+    path("brian", views.brian),
+    path("david", views.david),
+    path("<str:name>", views.greet)
 ]
--- assignment/SolutionSet/LectureDjango/Lecture3/hello/views.py
+++ assignment/SolutionSet/LectureDjango/Lecture3/hello/views.py
@@ -3,7 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 def brian(request):
     return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    context = {
+        "name": name.capitalize() 
+        }    
+    return render(request, "hello/greet.html", context)
