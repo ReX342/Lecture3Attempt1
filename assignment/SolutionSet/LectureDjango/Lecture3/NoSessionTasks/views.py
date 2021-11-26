9200c99948f2091348d4b75f9cf564f7a1bf952d
ReX342 <occ.rex@gmail.com>
2021-11-26 01:16:27 +0100

    broke my server doing tasks again

--- assignment/SolutionSet/LectureDjango/Lecture3/NoSessionTasks/views.py
+++ assignment/SolutionSet/LectureDjango/Lecture3/NoSessionTasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+tasks = [ "fe", "da", "ds"]
+
 # Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
